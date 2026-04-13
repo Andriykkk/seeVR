6f1a4ea67bb216ea045700fde1fe37093cf60c90
Andriykkk <andriykostenkostd@gmail.com>
2026-04-13 13:36:41 +0300

    delete old code and slightly change folder structure

--- src_zig/c.zig
+++
@@ -1,8 +0,0 @@
-// Single @cImport so all Vulkan/GLFW types are shared
-pub const c = @cImport({
-    @cDefine("VK_USE_PLATFORM_XLIB_KHR", "1");
-    @cDefine("GLFW_INCLUDE_VULKAN", "1");
-    @cInclude("GLFW/glfw3.h");
-    @cInclude("vulkan/vulkan.h");
-    @cInclude("shaderc/shaderc.h");
-});
